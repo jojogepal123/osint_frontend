69f97f0adcf58a328701d4257aac3d3687b1e93e
avigepal <avigepal57@gmail.com>
2025-05-28 20:25:04 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    minify:'terser',
+    sourcemap: false,
+    terserOptions:{
+      compress: true,
+      mangle: {
+        toplevel: true,
+        properties: true,
+      },
+      format:{
+        comments: false,
+      }
+    }
+  }
 })
